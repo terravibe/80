ed545b3702ca1ce079630647bf90d85a229785d3
balazs-foldesi <balazs.foldesi@gmail.com>
2019-10-23 19:54:06 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => {
+	res.send('Hello world!');
+});
+
+app.listen(port, () => {
+	// tslint:disable-next-line:no-console
+	console.log(`server started at http://localhost:${port}`);
+});
